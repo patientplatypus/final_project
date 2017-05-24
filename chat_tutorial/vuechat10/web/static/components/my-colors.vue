20052f5f7e781e8747c3212c7455dc667f3aa8b4
patientplatypus <pweyand@gmail.com>
2017-05-24 12:51:51 -0500

    done

---
+++ chat_tutorial/vuechat10/web/static/components/my-colors.vue
@@ -0,0 +1,480 @@
+
+
+<template>
+  <div class="my-colors">
+    <div class='rBlock' v-bind:style='styleObject'>
+      <p>
+        <input id="sliderR" v-on:mouseup="sendColor" v-model="numberR" type="range" min="0" max="255" step="1" />
+      </p>
+    </div>
+    <div class="gBlock">
+      <p>
+        <input id="sliderG" v-on:mouseup="sendColor" v-model="numberG" type="range" min="0" max="255" step="1" />
+      </p>
+    </div>
+    <div class="bBlock">
+      <p>
+        <input id="sliderB" v-on:mouseup="sendColor" v-model="numberB" type="range" min="0" max="255" step="1" />
+      </p>
+    </div>
+
+    <div class='pencildiv'>
+      <i id="pencil1" v-on:mousedown="setSize2" class="fa fa-pencil" style="border-width: 7px; border-style: solid; border-color:black; font-size:48px; background-color: #F9EBE0; border-radius: 5px;" aria-hidden="true"></i>
+      <i id="pencil2" v-on:mousedown="setSize4" class="fa fa-pencil" style="font-size:70px; background-color: #F9EBE0; border-radius: 5px;" aria-hidden="true"></i>
+      <i id="pencil3" v-on:mousedown="setSize8" class="fa fa-pencil" style="font-size:120px; background-color: #F9EBE0; border-radius: 5px;" aria-hidden="true"></i>
+    </div>
+
+<!-- http://i2.kym-cdn.com/profiles/icons/medium/000/065/488/RAINBOW%20DASH%20ICON.PNG -->
+<!-- http://i.imgur.com/hdzwfV9.png -->
+<!-- http://i.imgur.com/KF7SqDb.jpg -->
+<!-- http://i.imgur.com/jN4vzJn.jpg -->
+
+<!-- http://localhost:4000/rainbowdash.png
+http://localhost:4000/pukeyunicorn.jpg
+http://localhost:4000/platypusthumb.jpg -->
+
+    <div class='stickerdiv'>
+      <img v-on:mousedown="sticker1" id="sticker1" class="sticker" src='http://i.imgur.com/hdzwfV9.png'/>
+      <img v-on:mousedown="sticker2" id="sticker2" class="sticker" src='http://i.imgur.com/KF7SqDb.jpg'/>
+      <img v-on:mousedown="sticker3" id="sticker3" class="sticker" src='http://i.imgur.com/jN4vzJn.jpg'/>
+    </div>
+
+
+  </div>
+</template>
+
+
+<style scoped>
+
+  .pencildiv{
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  .stickerdiv{
+    text-align: center;
+    margin-top: 10px;
+  }
+
+  .sticker{
+    margin-right: 3px;
+  }
+
+  .my-colors {
+    display: inline-block;
+    float: right;
+    background-color: #208AAE;
+    height: 803px;
+    width: 20%;
+  }
+  .rBlock, .bBlock, .gBlock, .sizeBlock{
+    display: inline-block;
+    width: 100%;
+    height: 20%;
+    margin-left: 5%;
+    float:left;
+  }
+  #sliderR, #sliderG, #sliderB, #sliderS{
+    height: 40px;
+    width: 80%;
+  }
+
+  /*slider styling courtesy of http://danielstern.ca/range.css/#/*/
+
+
+  #sliderR {
+    -webkit-appearance: none;
+    width: 90%;
+    margin: 13.8px 0;
+    background: transparent;
+  }
+  #sliderR:focus {
+    outline: none;
+  }
+  #sliderR::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #f9ebe0;
+    border-radius: 1.3px;
+    border: 0.2px solid #000000;
+  }
+  #sliderR::-webkit-slider-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ff0000;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -14px;
+  }
+  #sliderR:focus::-webkit-slider-runnable-track {
+    background: #fdf9f5;
+  }
+  #sliderR::-moz-range-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #f9ebe0;
+    border-radius: 1.3px;
+    border: 0.2px solid #000000;
+  }
+  #sliderR::-moz-range-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ff0000;
+    cursor: pointer;
+  }
+  #sliderR::-ms-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  #sliderR::-ms-fill-lower {
+    background: #f5ddcb;
+    border: 0.2px solid #000000;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  #sliderR::-ms-fill-upper {
+    background: #f9ebe0;
+    border: 0.2px solid #000000;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  #sliderR::-ms-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #ff0000;
+    cursor: pointer;
+    height: 8.4px;
+  }
+  #sliderR:focus::-ms-fill-lower {
+    background: #f9ebe0;
+  }
+  #sliderR:focus::-ms-fill-upper {
+    background: #fdf9f5;
+  }
+
+
+  #sliderG {
+    -webkit-appearance: none;
+    width: 90%;
+    margin: 13.8px 0;
+    background: transparent;
+  }
+  #sliderG:focus {
+    outline: none;
+  }
+  #sliderG::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #f9ebe0;
+    border-radius: 1.3px;
+    border: 0.2px solid #000000;
+  }
+  #sliderG::-webkit-slider-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #00ff00;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -14px;
+  }
+  #sliderG:focus::-webkit-slider-runnable-track {
+    background: #fdf9f5;
+  }
+  #sliderG::-moz-range-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #f9ebe0;
+    border-radius: 1.3px;
+    border: 0.2px solid #000000;
+  }
+  #sliderG::-moz-range-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #00ff00;
+    cursor: pointer;
+  }
+  #sliderG::-ms-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  #sliderG::-ms-fill-lower {
+    background: #f5ddcb;
+    border: 0.2px solid #000000;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  #sliderG::-ms-fill-upper {
+    background: #f9ebe0;
+    border: 0.2px solid #000000;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  #sliderG::-ms-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #00ff00;
+    cursor: pointer;
+    height: 8.4px;
+  }
+  #sliderG:focus::-ms-fill-lower {
+    background: #f9ebe0;
+  }
+  #sliderG:focus::-ms-fill-upper {
+    background: #fdf9f5;
+  }
+
+  #sliderB {
+    -webkit-appearance: none;
+    width: 90%;
+    background: transparent;
+    margin: 13.8px 0;
+  }
+  #sliderB:focus {
+    outline: none;
+  }
+  #sliderB::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #f9ebe0;
+    border-radius: 1.3px;
+    border: 0.2px solid #000000;
+  }
+  #sliderB::-webkit-slider-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #0000ff;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -14px;
+  }
+  #sliderB:focus::-webkit-slider-runnable-track {
+    background: #fdf9f5;
+  }
+  #sliderB::-moz-range-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: #f9ebe0;
+    border-radius: 1.3px;
+    border: 0.2px solid #000000;
+  }
+  #sliderB::-moz-range-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #0000ff;
+    cursor: pointer;
+  }
+  #sliderB::-ms-track {
+    width: 100%;
+    height: 8.4px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  #sliderB::-ms-fill-lower {
+    background: #f5ddcb;
+    border: 0.2px solid #000000;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  #sliderB::-ms-fill-upper {
+    background: #f9ebe0;
+    border: 0.2px solid #000000;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  #sliderB::-ms-thumb {
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    border: 1px solid #000000;
+    height: 36px;
+    width: 16px;
+    border-radius: 3px;
+    background: #0000ff;
+    cursor: pointer;
+    height: 8.4px;
+  }
+  #sliderB:focus::-ms-fill-lower {
+    background: #f9ebe0;
+  }
+  #sliderB:focus::-ms-fill-upper {
+    background: #fdf9f5;
+  }
+
+
+
+</style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        numberR: 0,
+        numberG: 0,
+        numberB: 0,
+        numberA: 1,
+        lineSize: 2,
+        emitString: ""
+      }
+    },
+    methods: {
+      sendColor: function(){
+        this.emitString = "rgba("+this.numberR+","+this.numberG+","+this.numberB+","+this.numberA+")";
+        // var pencil1 = document.getElementById("pencil1");
+        // pencil1.setAttribute('style', color: emitstring);
+        document.getElementById("pencil1").style["color"] = this.emitString;
+        document.getElementById("pencil2").style["color"] = this.emitString;
+        document.getElementById("pencil3").style["color"] = this.emitString;
+
+        document.getElementById("pencil1").style["border-color"] = this.emitString;
+        document.getElementById("pencil2").style["border-color"] = this.emitString;
+        document.getElementById("pencil3").style["border-color"] = this.emitString;
+
+        console.log("emitString inside my-colors before send ",this.emitString);
+        this.$parent.colorSent = this.emitString;
+        this.$parent.sizeSent = this.lineSize;
+      },
+      setSize2: function(){
+
+        document.getElementById("pencil2").style["border-width"] = "0px";
+        document.getElementById("pencil3").style["border-width"] = "0px";
+
+        document.getElementById("sticker1").style["border-width"] = "0px";
+        document.getElementById("sticker2").style["border-width"] = "0px";
+        document.getElementById("sticker3").style["border-width"] = "0px";
+
+        document.getElementById("pencil1").style["border-width"] = "7px";
+        document.getElementById("pencil1").style["border-style"] = "solid";
+        document.getElementById("pencil1").style["border-color"] = this.emitString;
+
+        this.lineSize = 2;
+        this.sendColor();
+
+      },
+      setSize4: function(){
+
+        document.getElementById("pencil1").style["border-width"] = "0px";
+        document.getElementById("pencil3").style["border-width"] = "0px";
+
+        document.getElementById("sticker1").style["border-width"] = "0px";
+        document.getElementById("sticker2").style["border-width"] = "0px";
+        document.getElementById("sticker3").style["border-width"] = "0px";
+
+        document.getElementById("pencil2").style["border-width"] = "7px";
+        document.getElementById("pencil2").style["border-style"] = "solid";
+        document.getElementById("pencil2").style["border-color"] = this.emitString;
+
+        this.lineSize = 4;
+        this.sendColor();
+      },
+      setSize8: function(){
+
+        document.getElementById("pencil1").style["border-width"] = "0px";
+        document.getElementById("pencil2").style["border-width"] = "0px";
+
+        document.getElementById("sticker1").style["border-width"] = "0px";
+        document.getElementById("sticker2").style["border-width"] = "0px";
+        document.getElementById("sticker3").style["border-width"] = "0px";
+
+        document.getElementById("pencil3").style["border-width"] = "7px";
+        document.getElementById("pencil3").style["border-style"] = "solid";
+        document.getElementById("pencil3").style["border-color"] = this.emitString;
+
+        this.lineSize = 8;
+        this.sendColor();
+      },
+      // https://yt3.ggpht.com/-LQMKJ6Q021Q/AAAAAAAAAAI/AAAAAAAAAAA/5LCUrapagvo/s100-c-k-no-mo-rj-c0xffffff/photo.jpg
+     sticker1: function(){
+
+        document.getElementById("pencil1").style["border-width"] = "0px";
+        document.getElementById("pencil2").style["border-width"] = "0px";
+        document.getElementById("pencil3").style["border-width"] = "0px";
+
+        document.getElementById("sticker2").style["border-width"] = "0px";
+        document.getElementById("sticker3").style["border-width"] = "0px";
+
+        document.getElementById("sticker1").style["border-width"] = "7px";
+        document.getElementById("sticker1").style["border-style"] = "solid";
+        document.getElementById("sticker1").style["border-color"] = "black";
+
+        this.emitString = 'http://notahotdog.nanoapp.io/rainbowdash.png';
+        this.$parent.colorSent = this.emitString;
+     },
+     sticker2: function(){
+
+        document.getElementById("pencil1").style["border-width"] = "0px";
+        document.getElementById("pencil2").style["border-width"] = "0px";
+        document.getElementById("pencil3").style["border-width"] = "0px";
+
+        document.getElementById("sticker1").style["border-width"] = "0px";
+        document.getElementById("sticker3").style["border-width"] = "0px";
+
+        document.getElementById("sticker2").style["border-width"] = "7px";
+        document.getElementById("sticker2").style["border-style"] = "solid";
+        document.getElementById("sticker2").style["border-color"] = "black";
+
+        this.emitString = 'http://notahotdog.nanoapp.io/pukeyunicorn.jpg';
+        this.$parent.colorSent = this.emitString;
+     },
+     sticker3: function(){
+
+        document.getElementById("pencil1").style["border-width"] = "0px";
+        document.getElementById("pencil2").style["border-width"] = "0px";
+        document.getElementById("pencil3").style["border-width"] = "0px";
+
+        document.getElementById("sticker1").style["border-width"] = "0px";
+        document.getElementById("sticker2").style["border-width"] = "0px";
+
+        document.getElementById("sticker3").style["border-width"] = "7px";
+        document.getElementById("sticker3").style["border-style"] = "solid";
+        document.getElementById("sticker3").style["border-color"] = "black";
+
+        this.emitString = 'http://notahotdog.nanoapp.io/platypusthumb.jpg';
+        this.$parent.colorSent = this.emitString;
+     }
+   }
+  }
+</script>
---
+++ chat_tutorial/vuechat10/web/static/components/my-username.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="my-username">
+
+    <div class="usernameBlock">
+      <p>
+        <input id="inputname" placeholder="what's your name? :D" type="text" v-model="username" v-on:keyup.13="sendUsername">
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+  .my-username{
+    /*display: inline-block;
+    left: 0px;
+    top: 510px;
+    float: left;
+    color: #F9EBE0;
+    background-color: #0D2149;
+    margin:10px;
+    padding:10px;
+    width:auto;
+    position:absolute;*/
+    display: inline-block;
+    top: 80px;
+    left: 8px;
+    /*float: left;
+    clear: both;*/
+    word-wrap: break-word;
+    background-color: transparent;
+    color:#F9EBE0;
+    text-align: left;
+    width: 9.86%;
+    margin: 0;
+    padding: 0;
+    position:absolute;
+  }
+  #inputname{
+    background-color: #F9EBE0;
+    outline: none;
+    outline-width: 0;
+    border-width: 0;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  .usernameBlock h4{
+    color: #0D2149;
+  }
+</style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        username: ''
+      }
+    },
+    methods: {
+      sendUsername: function(){
+        // var \ = "rgba("+this.numberR+","+this.numberG+","+this.numberB+","+this.numberA+")";
+        // console.log("emitString inside my-colors before send ",emitString);
+        // this.$parent.colorSent = emitString;
+        console.log("sendUsername button has been pressed");
+        console.log('value of this.username ', this.username);
+        this.$parent.usernameSent = this.username;
+        console.log('value of this.$parent.usernameSent ', this.$parent.usernameSent);
+        this.username="";
+        var inputbox = document.getElementById('inputname');
+        inputbox.setAttribute('placeholder','name has been changed :)');
+      }
+   }
+  }
+</script>
